e31ea78c70dbd22d088fb3f2233fd4d47a21e630
Daniel <dct.lau17@gmail.com>
2018-09-30 16:11:32 +0100

    knows when not a leapyear

--- spec/leapYearSpec.js
+++ spec/leapYearSpec.js
@@ -1,5 +1,10 @@
 describe('leapYear', () => {
   it('returns leapyear when year is divisible by 400', () => {
-    expect(leapYear(400)).toEqual('Year 400 is a leap year')
+    expect(leapYear(2000)).toEqual('Year 2000 is a leap year')
   });
+
+  it('knows when year is not divisible by 400 but divisible by 100', () => {
+    expect(leapYear(1700)).toEqual('Year 1700 is NOT a leap year')
+  });
+
 });
--- src/leapYear.js
+++ src/leapYear.js
@@ -1,7 +1,15 @@
 function leapYear(year) {
-  return 'Year ' + year + ' is a leap year'
+  if(isDivisibleBy400(year) && isDivisibleBy100(year)) {
+    return 'Year ' + year + ' is a leap year'
+  } else {
+    return  'Year ' + year + ' is NOT a leap year'
+  }
 }
 
-// function leapYear(year) {
-//   return 'Year 400 is a leap year'
-// }
+function isDivisibleBy400(year) {
+  return year % 400 === 0
+}
+
+function isDivisibleBy100(year) {
+  return year % 100 === 0
+}
